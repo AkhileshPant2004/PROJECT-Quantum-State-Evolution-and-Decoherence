6800634ef14e73ef873d67ccc6068dc8a0465332
Akhilesh Pant <akhileshpant2004@gmail.com>
2026-06-30 01:19:35 +0530

    Fix: Added dynamic environment port binding for Linux containers

--- app.py
+++ app.py
@@ -209,4 +209,6 @@ def download_file(filename):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Railway passes a dynamic port variable to the system environment
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
